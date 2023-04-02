53c0fad19aea02c1862ed899995ab800c05622a2
bughht <bughht@outlook.com>
2023-04-02 23:30:19 +0800

    Hmm?

--- test/test_FlowPhantom.cpp
+++ test/test_FlowPhantom.cpp
@@ -4,23 +4,24 @@
 #include <SeqSimulator.h>
 #include <SeqLoader.h>
 #include <filesystem>
+#include "argparse.hpp"
 
 using Eigen::Vector2d;
 using std::string;
 using std::vector;
 namespace fs = std::filesystem;
 
-int main(void)
+int main(int argc, char **argv)
 {
     SeqLoader sq("sequences_MOLLI/MOLLI_533_TR2.8_FA35_FOV256_K64_thick8_center_first.yaml");
     FlowPhantom phantom(
         2,
         2,
         60.,
-        (vector<double>){1500., 1300., 1100., 900., 500.},
-        (vector<double>){200., 180, 160, 140, 100.},
+        (vector<double>){1500., 1500., 1500., 1500., 500.},
+        (vector<double>){200., 200, 200, 200, 100.},
         (vector<double>){0.0, 0.1, 0.2, 0.3},
-        (vector<double>){320., 320., 20.},
+        (vector<double>){256., 256., 20.},
         500000);
     string save_path = "img_MOLLI/Phantom_MOLLI_533_TR2.8_FA35_FOV256_K64_thick8_center_first";
     if (!fs::exists(save_path))
