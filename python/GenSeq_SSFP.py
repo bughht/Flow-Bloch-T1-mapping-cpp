Fixed sin wave fluctuation bug caused by spin overflow and b-ssfp

python/GenSeq_SSFP.py
@@ -21,7 +21,7 @@ class bSSFP:
         self.TR, self.FA, self.FOV, self.k_shape, self.slice_thickness, self.k_center_first, self.sign_alter, self.preparation = TR, FA, np.array(
             FOV), np.array(
                 k_shape
-            ), slice_thickness, k_center_first, sign_alter, preparation
+        ), slice_thickness, k_center_first, sign_alter, preparation
         self.data_process()
 
     def data_process(self):
@@ -144,5 +144,7 @@ if __name__ == "__main__":
     # ssfp.generate("TR2.8_FA20_FOV320_K64_center_first")
     # ssfp = bSSFP(2.8, 20, [320, 320], [64, 64], 10, False, True, True)
     # ssfp.generate("TR2.8_FA20_FOV320_K64")
-    ssfp = bSSFP(2.8, 35, [256, 256], [64, 64], 8, False, True, True)
-    ssfp.generate("TR2.8_FA35_FOV256_K64_thick8")
+    # ssfp = bSSFP(2.8, 35, [256, 256], [64, 64], 8, False, True, True)
+    # ssfp.generate("TR2.8_FA35_FOV256_K64_thick8")
+    ssfp = bSSFP(2.8, 35, [2, 2], [2, 2], 8, False, True, True)
+    ssfp.generate("TR2.8_FA35_FOV2_K2_thick8")

run.py
@@ -19,21 +19,25 @@ n_vessel_y = 5
 
 vessel_radius = 20
 
-n_particle = int(2e7)
+n_particle = int(3e7)
 
 # seq_path = "sequences_MOLLI/MOLLI_533_TR2.8_FA35_FOV256_K64_thick8_dt20_center_first.yaml"
+# seq_path = "sequences_MOLLI/TEST_SSFP_533_TR2.8_FA35_FOV256_K64_thick8_dt20.yaml"
 seq_path = "sequences_MOLLI/MOLLI_533_TR2.8_FA35_FOV256_K64_thick8_dt20.yaml"
+# seq_path = "sequences_MOLLI/MOLLI_533_TR2.8_FA35_FOV256_K64_thick8.yaml"
 
 # flow_speed_fast = np.linspace(0, 1, 16).astype(str)
 # flow_speed_slow = np.linspace(0, 0.01, 16).astype(str)
 
-flow_speed = np.linspace(0, 0.02, 2 * n_vessel_x * n_vessel_y).reshape(
+# flow_speed = np.linspace(0, 0.02, 2 * n_vessel_x * n_vessel_y).reshape(
+#     2, n_vessel_x * n_vessel_y).astype(str)
+flow_speed = np.linspace(0, 0.4, 2 * n_vessel_x * n_vessel_y).reshape(
     2, n_vessel_x * n_vessel_y).astype(str)
 # flow_speed = np.linspace(0, 0.01, 1 * 16).reshape(1, 16).astype(str)
 # flow_speed = np.linspace(0.9, 1.0, 16).reshape(1, 16).astype(str)
 # flow_speed = np.linspace(0.15, .17, 16).reshape(1, 16).astype(str)
 
-space = [256, 256, 16]
+space = [256, 256, 256]
 _space = [str(S) for S in space]
 
 
@@ -49,8 +53,9 @@ for exp_idx, fs in enumerate(flow_speed):
     _T1_tissue = T1_Tissue
     _T2_tissue = T2_Tissue
 
-    # save_path = "exp_result_fast/ID{}_Speed_min{}_max{}".format(
-    save_path = "exp_result_slow_1500_5x5/ID{}_Speed_min{}_max{}".format(
+    save_path = "exp_result_slow_longz/ID{}_Speed_min{}_max{}".format(
+        # save_path = "exp_result_ssfp_fast/ID{}_Speed_min{}_max{}".format(
+        # save_path = "exp_result_slow_1500_5x5/ID{}_Speed_min{}_max{}".format(
         # save_path = "exp_result_fast_1500_5x5_0_5/ID{}_Speed_min{}_max{}".format(
         # save_path = "exp_result/ID{}_Speed_min{}_max{}".format(
         exp_idx,