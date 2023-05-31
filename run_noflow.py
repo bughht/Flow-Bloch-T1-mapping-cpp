cfcac8aa127274c76113db2f520c38771c88411e
bughht <bughht@outlook.com>
2023-05-31 21:04:27 +0800

    Display demo

--- run_noflow.py
+++ run_noflow.py
@@ -7,22 +7,23 @@ run("mkdir log", shell=True)
 # run("build/src/T1-Mapping -h", shell=True)
 
 # T1_Blood = np.arange(1400, 2001, 100).tolist()
-T1_Blood = np.linspace(1000, 2000, 25)
-# T1_Blood = 1400
+# T1_Blood = np.linspace(1000, 2000, 25)
+T1_Blood = 1500
 T2_Blood = 50
 
 T1_Tissue = 1000
 T2_Tissue = 50
 
-n_vessel_x = 5
-n_vessel_y = 5
+n_vessel_x = 1
+n_vessel_y = 1
 
-vessel_radius = 20
+vessel_radius = 64
 
-n_particle = int(8e5)
+n_particle = int(1e5)
 
 # seq_path = "sequences_MOLLI/MOLLI_533_TR2.8_FA35_FOV256_K64_thick8_dt20_center_first.yaml"
-seq_path = "sequences_MOLLI/MOLLI_533_TR2.8_FA35_FOV256_K64_thick8_dt20.yaml"
+# seq_path = "sequences_MOLLI/MOLLI_533_TR2.8_FA35_FOV256_K64_thick8_dt20.yaml"
+seq_path = "sequences_MOLLI/MOLLI_533_TR2.8_FA35_FOV256_K64_thick8_Gz_dt20.yaml"
 
 # flow_speed_fast = np.linspace(0, 1, 16).astype(str)
 # flow_speed_slow = np.linspace(0, 0.01, 16).astype(str)
@@ -35,7 +36,7 @@ fs = [str(flow_speed) for _ in range(n_vessel_x * n_vessel_y)]
 # flow_speed = np.linspace(0.9, 1.0, 16).reshape(1, 16).astype(str)
 # flow_speed = np.linspace(0.15, .17, 16).reshape(1, 16).astype(str)
 
-space = [256, 256, 16]
+space = [256, 256, 4]
 _space = [str(S) for S in space]
 
 
@@ -46,8 +47,10 @@ def execute_cmd(cmd, idx):
 
 
 exp_idx = 0
-_T1_blood = [str(_) for _ in T1_Blood]
-_T2_blood = [str(T2_Blood) for _ in range(n_vessel_x * n_vessel_y)]
+# _T1_blood = [str(_) for _ in T1_Blood]
+# _T2_blood = [str(T2_Blood) for _ in range(n_vessel_x * n_vessel_y)]
+_T1_blood = str(T1_Blood)
+_T2_blood = str(T2_Blood)
 _T1_tissue = T1_Tissue
 _T2_tissue = T2_Tissue
 
@@ -57,11 +60,12 @@ _T2_tissue = T2_Tissue
 #     exp_idx,
 #     fs[0],
 #     fs[-1])
-save_path = "T1_1000_2000"
+save_path = "experiments/exp_MOLLI_no_flow"
 exp_exec = "build/src/T1-Mapping --T1_Blood {} --T2_Blood {} --T1_Tissue {} --T2_Tissue {} --n_vessel_xy {} {} --vessel_radius {} --n_particle {} --seq_path {} --save_path {} --flow_speed {} --space {}".format(
     " ".join(_T1_blood), " ".join(_T2_blood), _T1_tissue, _T2_tissue,
     n_vessel_x, n_vessel_y, vessel_radius, n_particle, seq_path, save_path,
     " ".join(fs), " ".join(_space))
+print(exp_exec)
 execute_cmd(exp_exec, exp_idx)
 # p = Process(target=execute_cmd, args=(exp_exec, exp_idx,))
 # p.start()
